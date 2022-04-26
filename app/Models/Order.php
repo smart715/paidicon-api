fb39234235719b53f42d1937675ccc96e27804f4
Ardasher Khojaev <ardagher.khojaev@tunelab.net>
2022-04-26 20:46:22 +0500

    Relations added to Order.php, User.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
     //public $timestamps = false;
     protected $fillable = [
-        'uuid', 'user_id', 'package_id', 'payment_method', 'transaction_id', 'amount',
-        'discount', 'discount_reason', 'total_payable', 'package_status', 'updated_by_user_id',
+        'uuid',
+        'user_id',
+        'package_id',
+        'payment_method',
+        'transaction_id',
+        'amount',
+        'discount',
+        'discount_reason',
+        'total_payable',
+        'package_status',
+        'updated_by_user_id',
         'status'
     ];
     protected $hidden = [
@@ -20,4 +31,19 @@ class Order extends Model
     protected $casts = [
         'uuid' => 'string'
     ];
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function package(): BelongsTo
+    {
+        return $this->belongsTo(Package::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
